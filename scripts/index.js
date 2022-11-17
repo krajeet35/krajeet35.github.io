2a93bdb3fdfac44361d7dc061af89cd7941d6f92
krajeet35 <krajeet35@gmail.com>
2022-11-17 09:46:57 +0530

    1

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,3 @@
-import{navbar1} from "./navbar.js";
+import {navbar1} from "./navbar.js";
 
 document.querySelector("#navbar2").innerHTML=navbar1();
--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,5 +1,10 @@
 let navbar1 = ()=>{
-    return ``;
+    return `<div><a href="/index.html">Home</a></div>
+    <div><a href="/about.html">About Me</a></div>
+    <div><a href="/skills.html">Skills</a></div>
+    <div><a href="/project.html">Project</a></div>
+    <div><a href="/contact.html">Contact</a></div>
+    <div><a  href="https://drive.google.com/file/d/1jQClVC-IUiNja_gGnmatJE7qd233HK71/view?usp=share_link">Resume</a></div>`;
 }
 
-export{navbar1};
+export {navbar1}
