6fd60ba13f5226b1dfd55c3d7f346cccd89c7d4f
krajeet35 <krajeet35@gmail.com>
2025-01-28 11:57:45 +0530

    Adding admit card generation section in Riverdale, In progress

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,8 @@
+document.querySelector("#RiverdaleSelector").addEventListener('change', function(){
+    let url = this.value;
+    window.open(url, '_blank');
+})
 
-document.querySelector("#btn").addEventListener(function(e){
+document.querySelector("#btn").addEventListener('submit', function(e){
     alert("Form submitted successfully!!!")
 });
