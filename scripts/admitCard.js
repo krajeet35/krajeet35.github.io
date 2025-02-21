def22d0a3d705b49c3acacf5cdcc6dbf30b667aa
krajeet35 <krajeet35@gmail.com>
2025-02-21 12:10:44 +0530

    added admit card

--- scripts/admitCard.js
+++ scripts/admitCard.js
@@ -5,40 +5,23 @@ document.querySelector("#submit").addEventListener("click", function(){
     let pdf= new jsPDF();
     
     // Getting the form data
-    let srNo=document.querySelector("#srNo").value;
+    let getDate=document.querySelector("#selectDate").value;
+    let [year,month,day]=getDate.split('-')
+    getDate=`${day}-${month}-${year}`
     let rollNo=document.querySelector("#rollNo").value;
     let name=document.querySelector("#name").value;
-    let fatherName=document.querySelector("#fatherName").value;
-    let motherName=document.querySelector("#motherName").value;
-    let dob=document.querySelector("#dob").value;
-    let gender;
-    let genderElement=document.getElementsByName("gender");
-    for(let i=0;i<genderElement.length;i++){
-        if(genderElement[i].checked){
-            gender=genderElement[i].value
-        }
-    };
-    let classEnroll=document.querySelector("#class").value
-    let phone=document.querySelector("#phone").value
-    let aadhar=document.querySelector("#aadhar").value
-    let email=document.querySelector("#email").value
-    let address=document.querySelector("#address").value
+    let fatherName=document.querySelector("#fatherName").value;    
+    let classEnroll=document.querySelector("#class").value    
     let upload=document.querySelector("#upload").value;
-    let checkbox=document.querySelector("#checkbox")
-    if(srNo=="" || rollNo=="" || name=="" || fatherName=="" || dob=="" || gender=="" || classEnroll=="Select Class" || phone=="" || upload==""){
+
+   
+    if(getDate=="" || rollNo=="" || name=="" || fatherName=="" || classEnroll=="Select Class" || upload==""){
         alert("Please fill the required fields");
         return false
     }
-    else if(address.length>80){
-        alert("Address length should be less than 80 characters")
-    }
-    else if(!checkbox.checked){
-        alert("You must agree to the declaration first.");
-        return false;
-    }
     else{
         // Create a new pdf
-        pdf.rect(5, 5, pdf.internal.pageSize.width - 10, pdf.internal.pageSize.height - 10, 'S');
+        pdf.rect(5, 5, pdf.internal.pageSize.width - 10, pdf.internal.pageSize.height/2, 'S');
 
         // Setting watermark
         let watermarkText = "RIVERDALE PUBLIC SCHOOL";
@@ -47,51 +30,79 @@ document.querySelector("#submit").addEventListener("click", function(){
         pdf.setTextColor(241, 245, 254);
         let textWidth = pdf.getTextDimensions(watermarkText).w;
         let textHeight = pdf.getTextDimensions(watermarkText).h;
-        pdf.text(watermarkText, pdf.internal.pageSize.getWidth() / 2 - textWidth / 2, pdf.internal.pageSize.getHeight() / 2);
+        pdf.text(watermarkText, pdf.internal.pageSize.getWidth() / 2 - textWidth / 2, (pdf.internal.pageSize.getHeight() / 2)/2);
 
         //setting Header
         pdf.setFont("Helvetica", "BoldOblique");
         pdf.setTextColor(0, 0, 0);
         let header1 = "RIVERDALE PUBLIC SCHOOL";
         textWidth = pdf.getTextDimensions(header1).w;
-        pdf.text(header1, pdf.internal.pageSize.getWidth() / 2 - textWidth / 2, 30)
+        pdf.text(header1, pdf.internal.pageSize.getWidth() / 2 - textWidth / 2, 20)
 
         pdf.setFont("Helvetica", "BoldOblique");
         pdf.setFontSize(20);
-        let header3 = "ADMISSION FORM";
+        let header3 = "Admit card for annual examination 2024-25";
         textWidth = pdf.getTextDimensions(header3).w;
-        pdf.text(header3, pdf.internal.pageSize.getWidth() / 2 - textWidth / 2, 50);
+        pdf.text(header3, pdf.internal.pageSize.getWidth() / 2 - textWidth / 2, 40);
 
         pdf.setFont("helvetica", "normal");
         pdf.setFontSize(12);
         let header2 = "MARWAN BHOJ, MUZAFFARPUR";
         textWidth = pdf.getTextDimensions(header2).w;
-        pdf.text(header2, pdf.internal.pageSize.getWidth() / 2 - textWidth / 2, 40)
+        pdf.text(header2, pdf.internal.pageSize.getWidth() / 2 - textWidth / 2, 30)
 
         pdf.setFontSize(14);
         pdf.setLineHeightFactor(1.5);
-        let body = `Sr.No.: ${srNo}\nStudent Name: ${name}\nFather's Name: ${fatherName}\nMother's Name: ${motherName}\nDate of Birth: ${dob}\nGender: ${gender}\nClass: ${classEnroll}\nPhone: ${phone}\nAadhar No.: ${aadhar}\nEmail: ${email}\nAddress: ${address}\n\nDeclaration:\nI have read the general rules and regulation of the school as given in the prospectus.\nI am satisfied that these are good and necessary for the institution.`;
-        pdf.text(body, 10, 70);
-
-        pdf.text("Management", 150, 190);
-        pdf.text("-------------------------------------------------------------------------------------------------------------------", 10, 200)
-
-        // Office Use only
-        let officeUse = "FOR OFFICE USE ONLY"
-        textWidth = pdf.getTextDimensions(officeUse).w;
-        pdf.text(officeUse, pdf.internal.pageSize.getWidth() / 2 - textWidth / 2, 210);
-        pdf.text("Mob: +91 9113711647", 140, 220)
-        pdf.text(`Sr.No.: ${srNo}`, 10, 230)
-        pdf.text(`Student Name: ${name}`, 10, 238);
-        if (gender == "Female") {
-            pdf.text(`D/o: ${fatherName}`, 10, 246);
+        let body = `Date: ${getDate}\nStudent Name: ${name}\nFather's Name: ${fatherName}\nClass: ${classEnroll}\nShift-I: 9:00 -10:00 AM\nShift-II: 10:00 -11:00 AM`;
+        pdf.text(body, 10, 50);
+        let yCordinate=90;
+        let xCordinate=10;
+        if(classEnroll == "Play" || classEnroll == "Nursery" || classEnroll == "LKG" || classEnroll == "UKG"){
+            let headers=['Date', 'Day', 'Shift-I', 'Shift-II']
+            let data1=[['24-02-2025','Monday','Hindi','English'],['25-02-2025','Tuesday','Mathematics','Drawing'],['27-02-2025','Thursday','Music','']]
+            headers.forEach(header => {
+                pdf.rect(xCordinate,yCordinate,40,10)
+                pdf.text(header, xCordinate+2, yCordinate+5);
+                xCordinate+=40
+            });
+            xCordinate=10;
+            yCordinate=100
+            data1.forEach(row=>{
+                row.forEach(cell=>{
+                    pdf.rect(xCordinate,yCordinate,40,10)
+                    pdf.text(cell, xCordinate+2,yCordinate+5)
+                    xCordinate+=40;
+                })
+                xCordinate=10;
+                yCordinate+=10
+            })
         }
-        else {
-            pdf.text(`S/o: ${fatherName}`, 10, 246);
+        else{
+            let headers=['Date', 'Day', 'Shift-I', 'Shift-II']
+            let data1=[['24-02-2025','Monday','Hindi','Sanskrit'],['25-02-2025','Tuesday','English','Mathematics'],['27-02-2025','Thursday','Science','S.S.T.'],['28-02-2025','Friday','G.K.','Computer']]
+            headers.forEach(header => {
+                pdf.rect(xCordinate,yCordinate,40,10)
+                pdf.text(header, xCordinate+2, yCordinate+5);
+                xCordinate+=40
+            });
+            xCordinate=10;
+            yCordinate=100
+            data1.forEach(row=>{
+                row.forEach(cell=>{
+                    pdf.rect(xCordinate,yCordinate,40,10)
+                    pdf.text(cell, xCordinate+2,yCordinate+5)
+                    xCordinate+=40;
+                })
+                xCordinate=10;
+                yCordinate+=10
+            })
         }
-        pdf.text(`Class: ${classEnroll}\nRoll No.: ${rollNo}`, 10, 254);
-        pdf.text("Received By", 160, 280);
 
+        pdf.text("Principal Signature", 10, 150);
+        pdf.text("Student Signature", 150, 150);
+        // pdf.text("-------------------------------------------------------------------------------------------------------------------", 10,170)
+
+       
         //Inserting image
         let image = document.querySelector("#upload");
         let file = image.files[0];
@@ -109,7 +120,7 @@ document.querySelector("#submit").addEventListener("click", function(){
                 let imgData = canvas.toDataURL('image/jpeg');
                 let imgWidth = 40;
                 let imgHeight = (imgWidth * img.height) / img.width;
-                pdf.addImage(imgData, "JPEG", 150, 60, imgWidth, imgHeight);
+                pdf.addImage(imgData, "JPEG", 150, 50, imgWidth, imgHeight);
                 pdf.save(`${name}.pdf`);
             }
         }
@@ -126,12 +137,12 @@ document.querySelector("#submit").addEventListener("click", function(){
 // limiting the image size
 let image=document.querySelector("#upload")
 image.onchange=function(){
-    let fileSize=Math.round(this.files[0].size/1024)
-    if(fileSize<50){
+    let fileSize=Math.round(this.files[0].size/2048)
+    if(fileSize<20){
         alert("Image size is too small.")
         this.value=""
     }
-    else if(fileSize>1200){
+    else if(fileSize>2049){
         alert("Image size is too big.")
         this.value=""
     }
